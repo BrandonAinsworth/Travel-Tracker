3f071ea2418fbbde5fb2f40f79e479706b5e29a1
Brandon Ainsworth <brandonaainsworth@gmail.com>
2022-06-11 18:54:10 -0600

    Add TravelerRepo and tests

--- src/TravelerRepo.js
+++ src/TravelerRepo.js
@@ -2,5 +2,9 @@ class TravelerRepo {
     constructor(allTravelerData){
     this.allTravelerData = allTravelerData;
     }
-    
+    returnSpecificTraveler(id){
+        let getUserFilter = this.allTravelerData.filter(user => id === user.id)
+        return getUserFilter[0]
+    }
 }
+export default TravelerRepo;
---
+++ test/TravelerRepo-test.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import TravelerRepo from '../src/TravelerRepo'
+import travelerSampleData from './Sample-traveler-data'
+
+describe ('TravelerRepo', () => {
+
+    let travelerRepo;
+  
+    beforeEach(() => {
+      travelerRepo = new TravelerRepo(travelerSampleData)
+    });
+  
+    it('should be a function', function () {
+      expect(TravelerRepo).to.be.a('function');
+    });
+
+    it(`should be an instance of Traveler`, () => {
+        expect(travelerRepo).to.be.instanceOf(TravelerRepo);
+      });
+    
+    it('should return a specific user by id', () => {
+        expect(travelerRepo.returnSpecificTraveler(6)).to.be.equal(travelerSampleData[5])
+    })
+});  
