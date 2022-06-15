8b3efad713757ef8a3827f6da0716401e566c7c4
Brandon Ainsworth <brandonaainsworth@gmail.com>
2022-06-14 19:15:49 -0600

    Calculate trips for year not functioning, dataRepo restructured

--- src/DataRepo.js
+++ src/DataRepo.js
@@ -1,23 +1,26 @@
 import Traveler from "./Traveler";
 import dayjs from "dayjs";
+import Destination from "./Destination";
+import Trip from "./Trips";
+import { use } from "chai";
 
 class DataRepo {
-    constructor(data){
+    constructor(data) {
         this.travelers = data[0]
         this.trips = data[1]
         this.destinations = data[2]
-        // this.currentTraveler = new Traveler()
+        this.currentTraveler;
         this.date = dayjs().format('YYYY/MM/DD')
     }
-    returnCurrentTravelerById(id){
+    returnCurrentTravelerById(id) {
         let travlerInfo = this.travelers.travelers.find(traveler => id === traveler.id)
         const traveler = new Traveler(travlerInfo)
         return traveler
     }
-    returnDestinationById(id){
+    returnDestinationById(id) {
         let destFilter = this.destinations.destinations.find(dest => {
-          return id === dest.id
-        }) 
+            return id === dest.id
+        })
         return destFilter
     }
     calculateTripCost(trip, destination) {
@@ -26,6 +29,23 @@ class DataRepo {
         let tripCost = lodging + flight;
         let totalWithAgentFee = Math.floor(tripCost * 1.10);
         return parseInt(totalWithAgentFee);
-        }
     }
+
+    calculateTotalSpentThisYear(id) {
+        const userTrips = this.trips.trips.filter(traveler => traveler.id === id)
+
+        let total = userTrips.reduce((acc, trip) => {
+            if (trip.date.includes('2022')) {
+                let destination = this.destinations.destinations.find(destination => {
+                    return (trip.destinationID === destination.id)
+                })
+                acc += this.calculateTripCost(trip, destination)
+            }
+            return acc
+        }, 0)
+        this.currentTraveler.totalSpentForYear = total
+        console.log('TOTAL', total)
+        return total
+    }
+}
 export default DataRepo;
